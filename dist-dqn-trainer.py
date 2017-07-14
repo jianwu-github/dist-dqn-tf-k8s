b098500f4e7cfaed08b5a7d30e4366931085d8fc
Jian Wu <jian.wu@samsung.com>
2017-07-14 01:59:59 -0700

    task_index as str to run in kubernetes cluster

--- dist-dqn-trainer.py
+++ dist-dqn-trainer.py
@@ -132,7 +132,7 @@ tf.app.flags.DEFINE_string("ps_hosts", "",
 tf.app.flags.DEFINE_string("worker_hosts", "",
                            "Comma-separated list of hostname:port pairs")
 tf.app.flags.DEFINE_string("job_name", "", "One of 'ps', 'worker'")
-tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
+tf.app.flags.DEFINE_string("task_index", "0", "Index of task within the job")
 tf.app.flags.DEFINE_string("log_path", "/tmp/train", "Log path")
 tf.app.flags.DEFINE_string("data_dir", "/data", "Data dir path")
 
@@ -181,19 +181,21 @@ def getDataReader():
 def main(_):
     ps_hosts = FLAGS.ps_hosts.split(",")
     worker_hosts = FLAGS.worker_hosts.split(",")
+    task_index = int(FLAGS.task_index)
+
     cluster = tf.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
     server = tf.train.Server(cluster,
                              job_name=FLAGS.job_name,
-                             task_index=FLAGS.task_index)
+                             task_index=task_index)
 
     if FLAGS.job_name == "ps":
         server.join()
     elif FLAGS.job_name == "worker":
-        is_chief = FLAGS.task_index == 0
-        checkpointDir = "/dqn-training-data/train_logs/worker-" + str(FLAGS.task_index)
+        is_chief = task_index == 0
+        checkpointDir = "/dqn-training-data/train_logs/worker-" + str(task_index)
 
         with tf.device(tf.train.replica_device_setter(
-                worker_device="/job:worker/task:%d" % FLAGS.task_index,
+                worker_device="/job:worker/task:%d" % task_index,
                 cluster=cluster)):
 
             global_step = tf.Variable(0, name='global_step', trainable=False)
@@ -267,7 +269,7 @@ def main(_):
             # restoring from a checkpoint, saving to a checkpoint, and closing when done
             # or an error occurs.
             with tf.train.MonitoredTrainingSession(master=server.target,
-                                                   is_chief=(FLAGS.task_index == 0),
+                                                   is_chief=(task_index == 0),
                                                    checkpoint_dir=checkpointDir,
                                                    hooks=session_hooks
                                                    ) as mon_sess:
@@ -296,10 +298,10 @@ def main(_):
 
                     mon_sess.run(target_update)
 
-                    print("The loss and global step at worker {} local step {} is {} and {}".format(FLAGS.task_index, i, loss_val, gs_val))
+                    print("The loss and global step at worker {} local step {} is {} and {}".format(task_index, i, loss_val, gs_val))
                     i += 1
 
-            print("Training on Worker {} has finished!".format(FLAGS.task_index))
+            print("Training on Worker {} has finished!".format(task_index))
 
 if __name__ == "__main__":
     tf.app.run()
