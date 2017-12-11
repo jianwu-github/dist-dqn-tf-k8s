9dc01a0ad3a78f44c82d0e823b8fc827f5165fb4
Jian Wu <hellojianwu@gmail.com>
2017-12-11 00:45:19 -0800

    add about directory

--- samples/sample_processor.py
+++ samples/sample_processor.py
@@ -5,6 +5,22 @@ import sys
 
 from datetime import datetime
 
+
+def process_sample_data():
+    """
+    Extract features from raw data:
+    1. age:       individual’s age
+    2. income:    income bracket
+    3. ngiftall:  number of gifts to date
+    4. numprom:   number of promotions to date
+    5. frequency: ngiftall / numprom
+    6. 
+
+    :return:
+    """
+    pass
+
+
 def main(args):
     pass
 
