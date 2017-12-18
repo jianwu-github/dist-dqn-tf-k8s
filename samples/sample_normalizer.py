e2860edae17539b980281da8254284aff4168e03
Jian Wu <hellojianwu@gmail.com>
2017-12-18 00:40:13 -0800

    working on sample normalizer ...

---
+++ samples/sample_normalizer.py
@@ -0,0 +1,17 @@
+import csv
+import sys
+
+import numpy as np
+
+
+
+
+def main(args):
+    input_data_file = "data/training_data.csv"
+    output_data_file = "data/normalized_training_data.csv"
+
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+    sys.exit(0)
