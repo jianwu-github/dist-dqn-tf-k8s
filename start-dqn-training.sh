39148780145c445f3bf8e8b28f10425221745c7c
Jian Wu <jian.wu@samsung.com>
2018-01-23 16:13:15 -0800

    debugging and testing dqn ...

--- start-dqn-training.sh
+++ start-dqn-training.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-python /dist-dqn-trainer.py $@
+python3 dist-dqn-trainer.py --ps_hosts=localhost:2222 --worker_hosts=localhost:2223 --job_name=ps --task_index=0 &
+
+python3 dist-dqn-trainer.py --ps_hosts=localhost:2222 --worker_hosts=localhost:2223 --job_name=worker --task_index=0
